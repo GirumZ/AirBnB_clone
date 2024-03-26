61b31838cc3b50107689e87f0a919e1639cf4c53
GirumZ <girum.zebe@gmail.com>
2024-03-26 19:54:59 +0300

    [Update]: Updated the test cases

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,60 +1,66 @@
 #!/usr/bin/python3
-""" Python module for the FileStorage class tests"""
-from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
-from models import storage
-import unittest
+""" Test module for file_storage module"""
 import os
 import json
-import datetime
+import unittest
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models.user import User
+import models
 
 
-class TestFileStorageClass(unittest.TestCase):
-    """ Tests for the FileStorage class"""
-
+class TestFileStorage(unittest.TestCase):
+    """ Test for FileStorage class"""
     def setUp(self):
-        """ Sets up the testing environment"""
+        """ Sets up the test environment"""
+        self.storage = FileStorage()
+        FileStorage._FileStorage__file_path = "test.json"
+        FileStorage._FileStorage__objects = {}
+        self.storage.reload()
 
-        self.model_1 = BaseModel()
-        self.model_2 = BaseModel()
-        self.model_3 = BaseModel()
+    def tearDown(self):
+        """ Cleansup after the test is complete"""
+        if os.path.exists("test.json"):
+            os.remove("test.json")
 
-    def test_BaseModel_save(self):
-        """ Tests the BaseModel save method"""
+    def test_file_path(self):
+        """ tests the existance and type of file path"""
+        self.assertEqual(hasattr(FileStorage, "_FileStorage__file_path"), True)
+        self.assertIsInstance(FileStorage._FileStorage__file_path, str)
+        self.assertEqual(FileStorage._FileStorage__objects, {})
 
-        self.model_1.save()
+    def test_objects(self):
+        """ Tests the attribute __objects"""
 
-        key = self.model_1.__class__.__name__ + "." + self.model_1.id
-        self.assertEqual(storage._FileStorage__objects[key], self.model_1)
-        self.assertIsInstance(self.model_1.id, str)
-        self.assertIsInstance(self.model_1.created_at, datetime.datetime)
-        self.assertIsInstance(self.model_1.updated_at, datetime.datetime)
+        self.assertIsInstance(FileStorage._FileStorage__objects, dict)
+        self.assertEqual(FileStorage._FileStorage__objects, {})
 
-    def test___file_path(self):
-        """ Tests the __file_path attribute"""
+    def test_all_property(self):
+        """ Tests the nethod all"""
+        self.assertIsInstance(models.storage.all(), dict)
+        self.assertEqual(self.storage.all(), {})
 
-        # checks if the __file_path attribute is a string
-        self.assertIsInstance(storage._FileStorage__file_path, str)
-
-    def test___objects(self):
-        """ Tests the __objects attribute"""
-
-        # checks if the __object attribute is a dictionary
-        self.assertIsInstance(storage._FileStorage__objects, dict)
-
-    def test_all(self):
-        """ Tests the all method of the FileStorage class"""
-
-        returned_value = storage.all()
-        self.assertEqual(returned_value, storage._FileStorage__objects)
+    def test_all_instance(self):
+        """ Tests the all method with instances"""
+        instance1 = BaseModel()
+        self.assertEqual(self.storage.all(), {
+            "BaseModel." + instance1.id: instance1
+        })
 
     def test_new(self):
-        """ Tests the new method of the FileStorage class"""
+        """ Tests the method new"""
+        n_dict = {
+                'id': '56d43177-cc5f-4d6c-a0c1-e167f8c27337',
+                'created_at': '2017-09-28T21:03:54.052298',
+                '__class__': 'BaseModel',
+                'updated_at': '2017-09-28T21:03:54.052302'
+        }
 
-        storage.new(self.model_1)
-        key = self.model_1.__class__.__name__ + "." + self.model_1.id
-        self.assertIn(key, storage._FileStorage__objects)
-        self.assertEqual(storage._FileStorage__objects[key], self.model_1)
+        k = "BaseModel.56d43177-cc5f-4d6c-a0c1-e167f8c27337"
+        instance = BaseModel(**n_dict)
+        self.storage.new(instance)
+        self.assertIn(k, self.storage.all())
+        self.assertIs(self.storage.all()[k], instance)
 
     def test_save(self):
         """ Tests the method save"""
@@ -68,8 +74,8 @@ class TestFileStorageClass(unittest.TestCase):
         k = "BaseModel.56d43177-cc5f-4d6c-a0c1-e167f8c27337"
         saved_as = {k: n_dict}
         instance = BaseModel(**n_dict)
-        storage.new(instance)
-        storage.save()
+        self.storage.new(instance)
+        self.storage.save()
         with open("test.json", "r") as f:
             got_dict = json.load(f)
             self.assertEqual(got_dict, saved_as)
@@ -85,9 +91,9 @@ class TestFileStorageClass(unittest.TestCase):
 
         k = "BaseModel.56d43177-cc5f-4d6c-a0c1-e167f8c27337"
         instance = BaseModel(**n_dict)
-        storage.new(instance)
-        storage.save()
-        storage.reload()
+        self.storage.new(instance)
+        self.storage.save()
+        self.storage.reload()
         self.assertEqual(len(self.storage.all().keys()), 1)
         self.assertIn(k, self.storage.all())
         self.assertEqual(self.storage.all()[k].to_dict(), n_dict)
